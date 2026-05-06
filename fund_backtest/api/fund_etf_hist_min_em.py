2ca541390d5ebd8bf3072acd477e75313eb23028
wuzeyu <wuzeyu2016@gmail.com>
2026-05-06 14:22:38 +0800

    delete fund_backtest

--- fund_backtest/api/fund_etf_hist_min_em.py
+++
@@ -1,6 +0,0 @@
-import akshare as ak
-import pandas as pd
-
-df = ak.fund_etf_hist_min_em(symbol="159952", period="1", adjust="", start_date="2024-03-20 09:30:00", end_date="2024-03-20 17:40:00")
-print(df)
-print(f"数据量: {len(df)} 条")
